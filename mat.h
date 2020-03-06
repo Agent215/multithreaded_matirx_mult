9d830f6d7663b714ecb6ef7b16839d12313d7f5c
Evan Fiordeliso <evan.fiordeliso@temple.edu>
2020-03-06 15:43:03 -0500

    Add the new vectorized function to the header

--- mat.h
+++ mat.h
@@ -10,6 +10,10 @@ int mmult(double *c,
           double *a, int aRows, int aCols, 
           double *b, int bRows, int bCols);
 
+int mmult_vectorized(double *c, 
+          double *a, int aRows, int aCols, 
+          double *b, int bRows, int bCols);
+
 int mmult_omp(double *c,
 		      double *a, int aRows, int aCols,
 		      double *b, int bRows, int bCols);
