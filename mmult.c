d63fbf8ba57d2b102f264f3e93c11fc2911bbc64
Evan James Fiordeliso <tug91450@chinook.cis.temple.edu>
2020-03-06 15:36:24 -0500

    Added vectorized form of matrix multiplication algo

--- mmult.c
+++ mmult.c
@@ -36,5 +36,32 @@ int mmult(double *c,
   return 0;
 }
 
+/**
+ * An algorithm for matrix multiplication that allows for vectorization.
+ * 
+ * @param c : the matrix in which to place the result of the matrix multiplication.
+ * @param a : the first matrix.
+ * @param aRows : the number of rows in a.
+ * @param aCols : the number of columns in a.
+ * @param b : the second matrix.
+ * @param bRows : the number of rows in b.
+ * @param bCols : the number of columns in b.
+ * @return 0 if the matrix multiplication is successful.
+ */
+int mmult_vectorized(double *c, 
+	      double *a, int aRows, int aCols, 
+	      double *b, int bRows, int bCols) {
 
+    for(int i = 0; i < aRows; ++i) {
+       for(int k = 0; k < bCols; k++)
+          c[i * bCols + k] = 0; 
 
+       for(int j = 0; j < bCols; ++j) {
+            for(int k = 0; k < aRows; ++k) {
+                c[i * bCols + j] += a[i * aRows + k] * b[k * bCols + j];
+            }
+        }
+    }
+
+  return 0;
+}
